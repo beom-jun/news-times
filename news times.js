4a29a337822f84cf12d7f95b4aca14f0d4ceeb73
chlrhwns1 <77625876+beom-jun@users.noreply.github.com>
2024-02-20 14:03:31 +0900

    news times.js

--- news times.js
+++ news times.js
@@ -2,7 +2,7 @@ const API_KEY = `d442e5b521b24b44b3eff15add9db6bd`;
 let news=[];
 
 const getLastesnews = async ()=>{
-    const url=new URL(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`
+    const url=new URL(`https://benevolent-toffee-df4f4d.netlify.app//top-headlines?country=us&apiKey=${API_KEY}`
     ); 
     const response =await fetch(url);
     const data = await response.json()
